10a208c22e5524dd8e979ca08fe13067ae4cf53d
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-12-12 10:35:30 +0100

    new me

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -6,24 +6,14 @@ type Data = {
   success: boolean;
 };
 
-// require('dotenv').config()
-
-// const sgMail = require('@sendgrid/mail')
-
-// const {SG_API_KEY, FROM_EMAIL, TO_EMAIL} = process.env
-
-// sgMail.setApiKey(SG_API_KEY)
-
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // const {name, email, message} = req.body
-  // res.status(400).json({message: 'Bad request'})
   if (req.method === 'POST') {
     const data = req.body;
     const { name, email, message } = data;
     try {
       await transporter.sendMail({
         ...mailOptions,
-        subject: 'Nextjs contact form',
+        subject: 'Real Estate Trade Contact Form',
         html: `<p><strong>Name: </strong>${name}</p>
                 <p><strong>Email: </strong>${email}</p>
                 <p><strong>Message: </strong>${message}</p>`,
